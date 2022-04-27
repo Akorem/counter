c9b152d11c69af8b495f15122dab297a7c68256d
akorem <giftnkor@gmail.com>
2022-04-27 10:56:16 +0100

    commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let header = document.querySelector('h1');
+let dec = document.querySelector('.dec');
+let res = document.querySelector('.res');
+let inc = document.querySelector('.inc');
+let body = document.querySelector('body')
+
+function decrease (){
+    header.textContent--
+}
+
+dec.addEventListener('click', decrease)
+
+
+function reset (){
+    header.textContent = 0
+}
+
+res.addEventListener('click', reset)
+
+
+function increase (){
+    header.textContent++
+}
+
+inc.addEventListener('click', increase)
+
+
+let Dex = () => {
+    body.style.backgroundColor = 'yellow'
+} 
+dec.addEventListener('click', Dex)
+
+let Rex = () => {
+    body.style.backgroundColor = 'red'
+} 
+res.addEventListener('click', Rex)
+
+let incx = () => {
+    body.style.backgroundColor = 'green'
+} 
+inc.addEventListener('click', incx)
+
+
+
+
+// alert('hi')
+
